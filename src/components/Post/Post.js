6a007b30d5a141947c896a9fb0b5c69d80dca2fc
Ivana Livaja <ivana.livaja@hotmail.com>
2021-05-14 00:20:09 +0200

    bugfix localSorage

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -12,18 +12,20 @@ import './Post.css';
 
   console.log(`${propsmessage} Post`);
 
-  localStorage.setItem('id', id);
-  localStorage.setItem('title', title);
-  localStorage.setItem('text', text);
-  localStorage.setItem('author', authorsName.name);
+  const handleClick = () => {
+    localStorage.setItem('id', id);
+    localStorage.setItem('title', title);
+    localStorage.setItem('text', text);
+    localStorage.setItem('author', authorsName.name);
+  }
 
   return (
       <div className="Post">
         <h1 className="Post__title">{title}</h1>
         <div className="Post__name">{authorsName.name}</div>
         <p className="Post__text">{text}  <Link  className="Post__link"
-        to={{ pathname: `/post/${id}` }} target="_blank" rel="noopener noreferrer">Read more...</Link></p>
-        <button className="Posts__button" onClick={() => {DataCtx.getComments(id)}}>Show Comments</button>
+        to={{ pathname: `/post/${id}` }} target="_blank" rel="noopener noreferrer" onClick={handleClick}>Read more...</Link></p>
+        <button className="Posts__button" onClick={() => {DataCtx.getComments(id)}}> Show Comments</button>
       </div>
   );
 });
